f669c44d31647206305dcc5f7599bc3654b1f198
Hesham Orabi <Hesham.Shawky1@outlook.com>
2023-02-21 19:26:51 +0200

    Main

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts() =>
+            "A List Of Products";
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id) =>
+            "A List Of Products";
+    }
+}
